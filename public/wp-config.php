8efb67e3ef772bf19ccf67a4a63b79dbc4d6ea7a
Carl Knapp <kn4ppster@gmail.com>
2020-06-15 20:10:42 +0100

    Remove composer dependency

--- public/wp-config.php
+++ public/wp-config.php
@@ -1,41 +1,4 @@
 <?php
-
-$root_dir = dirname(__DIR__);
-$webroot_dir = $root_dir.'/public';
-require_once $root_dir.'/vendor/autoload.php';
-
-// Load environment variables.
-$dotenv = Dotenv\Dotenv::createImmutable($root_dir);
-
-if (file_exists($root_dir . '/.env')) {
-    $dotenv->load();
-    $dotenv->required([
-        'WP_HOME'
-    ]);
-
-    if (!getenv('DB_HOST')) {
-        $dotenv->required([
-            'DB_NAME'
-            , 'DB_USER'
-            , 'DB_PASSWORD'
-        ]);
-    }
-}
-
-// Custom content URLs.
-if (!defined('WP_HOME') && getenv('WP_HOME')) {
-    define('WP_HOME', getenv('WP_HOME'));
-}
-if (!defined('WP_SITEURL') && defined('WP_HOME')) {
-    define('WP_SITEURL', WP_HOME.'/wp');
-}
-if (!defined('WP_CONTENT_DIR')) {
-    define('WP_CONTENT_DIR', dirname(__FILE__).'/'.getenv('CONTENT_NAME'));
-}
-if (!defined('WP_CONTENT_URL') && defined('WP_HOME')) {
-    define('WP_CONTENT_URL', WP_HOME.'/'.getenv('CONTENT_NAME'));
-}
-
 /**
  * The base configuration for WordPress
  *
@@ -50,29 +13,29 @@ if (!defined('WP_CONTENT_URL') && defined('WP_HOME')) {
  * * Database table prefix
  * * ABSPATH
  *
- * @link https://codex.wordpress.org/Editing_wp-config.php
+ * @link https://wordpress.org/support/article/editing-wp-config-php/
  *
  * @package WordPress
  */
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wordpress');
+define( 'DB_NAME', 'wordpress');
 
 /** MySQL database username */
-define('DB_USER', 'wordpress');
+define( 'DB_USER', 'wordpress');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'wordpress');
+define( 'DB_PASSWORD', 'wordpress');
 
 /** MySQL hostname */
-define('DB_HOST', 'db:3306');
+define( 'DB_HOST', 'db:3306');
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
+define( 'DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
+define( 'DB_COLLATE', '');
 
 /**#@+
  * Authentication Unique Keys and Salts.
@@ -83,14 +46,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY', getenv('AUTH_KEY'));
-define('SECURE_AUTH_KEY', getenv('SECURE_AUTH_KEY'));
-define('LOGGED_IN_KEY', getenv('LOGGED_IN_KEY'));
-define('NONCE_KEY', getenv('NONCE_KEY'));
-define('AUTH_SALT', getenv('AUTH_SALT'));
-define('SECURE_AUTH_SALT', getenv('SECURE_AUTH_SALT'));
-define('LOGGED_IN_SALT', getenv('LOGGED_IN_SALT'));
-define('NONCE_SALT', getenv('NONCE_SALT'));
+define( 'AUTH_KEY',         'c8863080bdbb76c3713c2f18ef3d11b5bb04aba9');
+define( 'SECURE_AUTH_KEY',  '4e7be37d50cbdb35c1d448846c696f85998ff522');
+define( 'LOGGED_IN_KEY',    'ef62a331d418688bb1e08ed0421fa70fcc58f97a');
+define( 'NONCE_KEY',        '4a849f25fa0560679538e99b745a4d9803176401');
+define( 'AUTH_SALT',        '7f23d9ff1387ade09659e3dc85e15e22333170fa');
+define( 'SECURE_AUTH_SALT', 'dfc4a02f943e86d517ddf9003ed1fe6f740dd214');
+define( 'LOGGED_IN_SALT',   '0eac563d2d5a559c1987abd51793f6621fb44739');
+define( 'NONCE_SALT',       'f10fa8f642f7f94b13e4d18759f71ca7de262cc3');
 
 /**#@-*/
 
@@ -100,7 +63,7 @@ define('NONCE_SALT', getenv('NONCE_SALT'));
  * You can have multiple installations in one database if you give each
  * a unique prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix = getenv('DB_PREFIX')?: 'wp_';
+$table_prefix = 'wp_';
 
 /**
  * For developers: WordPress debugging mode.
@@ -110,19 +73,24 @@ $table_prefix = getenv('DB_PREFIX')?: 'wp_';
  * in their development environments.
  *
  * For information on other constants that can be used for debugging,
- * visit the Codex.
+ * visit the documentation.
  *
- * @link https://codex.wordpress.org/Debugging_in_WordPress
+ * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
-define('WP_DEBUG', getenv('WP_DEBUG')?: false);
-define('WP_DEBUG_LOG', getenv('WP_DEBUG_LOG')?: false);
+define( 'WP_DEBUG', false );
+
+// If we're behind a proxy server and using HTTPS, we need to alert WordPress of that fact
+// see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+	$_SERVER['HTTPS'] = 'on';
+}
 
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {
-    define( 'ABSPATH', dirname( __FILE__ ) . '/wp/' );
+	define( 'ABSPATH', __DIR__ . '/' );
 }
 
 /** Sets up WordPress vars and included files. */
-require_once( ABSPATH . 'wp-settings.php' );
+require_once ABSPATH . 'wp-settings.php';
